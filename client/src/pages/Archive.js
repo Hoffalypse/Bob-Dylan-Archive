138c8a6d9e16ce482a5d551462f513b8e45d4205
Bryan Wienhoff <bhoff1980@gmail.com>
2022-12-23 05:27:10 -0700

    completed layout of archives page

--- client/src/pages/Archive.js
+++ client/src/pages/Archive.js
@@ -1,11 +1,62 @@
-
-import '../styles/archive.css'
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import ReactPlayer from "react-player/lazy";
+import "../styles/archive.css";
 
 function Archive() {
-  return( 
-<div className='bkgclr'>
-  <p className='larger'>Welcome to the Archives!</p>
- </div>
-  )
+  return (
+    <div className="bkgclr">
+      <p className="larger">Welcome to the Archives!</p>
+
+      <Container style={{ color: "white" }}>
+        <Row>
+          <Col>
+          <Form>
+            <p style={{textAlign:'center'}}>Find a Song </p>
+            <Form.Select aria-label="Default select example"style={{fontWeight:'bolder', marginBottom:'25px'}}>
+              <option>Song Title</option>
+              <option value="1">Don't Think Twice</option>
+              <option value="2">Hurricane</option>
+              <option value="3">Billy #1</option>
+            </Form.Select>
+
+            <Form.Select aria-label="Default select example"style={{fontWeight:'bolder', marginBottom:'25px'}}>
+              <option>Year Performed</option>
+              <option value="1">2022</option>
+              <option value="2">2021</option>
+              <option value="3">1984</option>
+            </Form.Select>
+
+            <Form.Select aria-label="Default select example"style={{fontWeight:'bolder', marginBottom:'25px'}}>
+              <option>Venue</option>
+              <option value="1">Carnegie Hall</option>
+              <option value="2">Grand Ole Opry</option>
+              <option value="3">Mission Ballroom</option>
+            </Form.Select>
+            <Button className="play">Watch</Button>
+            </Form>
+          </Col>
+          <Col>
+            <ReactPlayer
+              className="player"
+              url="https://www.youtube.com/watch?v=i8z7KzB16Ik"
+              playing="true"
+            />
+          </Col>
+          <Col >
+            <p style={{ textAlign: "center" }}>Now Playing</p>
+          <ul className="now-playing">
+            <li> Visions of Johanna</li>
+            <li> Portsmouth, UK</li>
+            <li> Summer 2000 </li>
+            <li> The Never Ending Tour </li>
+          </ul>
+          
+          </Col>
+          
+        </Row>
+      </Container>
+    </div>
+  );
 }
 export default Archive;
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -7,12 +7,9 @@ function Homepage () {
     return (
         <div className='bkclr'>
             <p className='larger'> Welcome to the Dylan Archives</p>
-            <ReactPlayer className='player' url='https://www.youtube.com/watch?v=a6Kv0vF41Bc'
+            <ReactPlayer className='player' url='https://www.youtube.com/watch?v=Mt4XbGPsoSA'
             playing='true' />
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
+           
         </div>
     )
 
