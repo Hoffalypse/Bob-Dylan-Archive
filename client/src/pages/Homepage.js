115f33e68bb8772d05c1fa6b47b38b33f003397e
Bryan Wienhoff <bhoff1980@gmail.com>
2022-12-23 05:43:45 -0700

    added some text to the homepage

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,16 +1,30 @@
-
+import { Container, Row, Col, Button } from "react-bootstrap";
 import '../styles/homepage.css'
 import ReactPlayer from 'react-player/lazy'
 
 function Homepage () {
 
     return (
-        <div className='bkclr'>
-            <p className='larger'> Welcome to the Dylan Archives</p>
+    <div className='bkclr'>
+        <p className='larger'> Welcome to the Dylan Archives</p>
+         <Container style={{ color: "white" }}>
+        <Row>
+          <Col>
+          <p>Some text here about why this site was created</p>
+          </Col>
+          <Col>
+            
             <ReactPlayer className='player' url='https://www.youtube.com/watch?v=Mt4XbGPsoSA'
             playing='true' />
+            </Col>
+            <Col> 
+            <p>Some text here about it being not for profit and donations are welcome</p>
+            <Button className="play">Donate</Button>
+            </Col>
+            </Row>
+            </Container>
            
-        </div>
+    </div>
     )
 
 }
